84974981c07bde15f2b6d4f1ac0a2358ce873d5a
tanakancolle <tanakancolle@gmail.com>
2017-02-06 18:04:47 +0900

    エディタウィンドウから生成出来るように変更

--- Assets/Scripts/Editor/PlantUMLConvertWindow.cs
+++ Assets/Scripts/Editor/PlantUMLConvertWindow.cs
@@ -5,18 +5,37 @@ using UnityEditor;
 
 public class PlantUMLConvertWindow : EditorWindow {
 
+    private static PlantUMLConvertWindow umlWindow;
+
     [MenuItem("UML/ConvertWindow")]
     static private void CreateWindow()
     {
-        var window = EditorWindow.GetWindow<PlantUMLConvertWindow> ();
+        if (umlWindow == null) {
+            umlWindow = CreateInstance<PlantUMLConvertWindow> ();
+        }
 
-        window.titleContent = new GUIContent ("PlantUMLConverter");
-        window.Show ();
+        umlWindow.titleContent = new GUIContent ("PlantUMLConverter");
+        umlWindow.ShowUtility ();
     }
 
-    private void OnGUI() {
-        GUILayout.Label ("対象クラス図");
+    private TextAsset textAsset;
+    private string createPath = "Assets/";
 
+    private void OnGUI() {
+        EditorGUILayout.BeginHorizontal ();
+        EditorGUILayout.LabelField ("対象クラス図", GUILayout.MaxWidth (100));
+        textAsset = EditorGUILayout.ObjectField (textAsset, typeof(TextAsset)) as TextAsset;
+        EditorGUILayout.EndHorizontal ();
+
+        EditorGUILayout.BeginHorizontal ();
+        EditorGUILayout.LabelField ("生成フォルダ", GUILayout.MaxWidth (100));
+        createPath = EditorGUILayout.TextField (createPath);
+        EditorGUILayout.EndHorizontal ();
+
+        if (GUILayout.Button ("生成開始")) {
+            var converter = new PlantUMLConverter ();
+            converter.ConvertProcess (textAsset.text, createPath);
+        }
     }
 }
 
